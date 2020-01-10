dfb6324b0bb54199be79777c95dab48add8827ee
unknown <jacob.laster23@gmail.com>
2020-01-10 18:51:09 -0500

    fixed spelling and added an attribute to Role, giving each role a list of trained employees. this implementation is broken, as a private variable is accessed directly, but I'm done for today.

--- DijkstraScheduler/Scheduler.cpp
+++ DijkstraScheduler/Scheduler.cpp
@@ -6,29 +6,53 @@
 #include "rapidxml/rapidxml.hpp"
 #include "rapidxml/rapidxml_utils.hpp"
 #include "Scheduler.h"
+#include "Employee.h"
+#include "Role.h"
 
 using namespace std;
 using namespace rapidxml;
 
-Scheduler::Scheduler() {
-	roleVector = vector<string>();
-	roleMap = map<string, int>();
+vector<string> getVectorFromFileName(string fileName) {
+	vector<string> v = vector<string>();
 
 	xml_node<>* node;
 	xml_attribute<>* attr;
-	file<> xml(roleFile.c_str);
+	file<> xml(fileName.c_str);
 	xml_document<> d;
 	d.parse<0>(xml.data());
 
-	node = d.first_node("roles");
+	node = d.first_node();
 	if (node == 0)
-		return;
+		return v;
 
 	for (attr = node->first_attribute(); attr; attr = attr->next_attribute())
-		roleVector.push_back(string(attr->value()));
+		v.push_back(string(attr->value()));
 
+	return v;
+}
+
+map<string, int> getMapFromVector(vector<string> v) {
+	map<string, int> m = map<string, int>();
+
+	for (int i = 0; i < v.size(); i++)
+		m.insert(pair<string, int>(v[i], i));
+
+	return m;
+}
+
+Scheduler::Scheduler() {
+	roleVector = getVectorFromFileName(roleFile);
+	employeeIDs = getVectorFromFileName(idFile);
+	roleMap = getMapFromVector(roleVector);
+	idMap = getMapFromVector(employeeIDs);
+
+	employees = vector<Employee>();
+	for (int i = 0; i < employeeIDs.size(); i++)
+		employees.push_back(Employee(employeeIDs[i] + ".xml", roleMap));//what if the employee knows a removed role?
+
+	roles = vector<Role>();
 	for (int i = 0; i < roleVector.size(); i++)
-		roleMap.insert(pair<string, int>(roleVector[i], i));
+		roles.push_back(Role(roleVector[i] + ".xml", employees, i));
 
 	return;
 }
--- DijkstraScheduler/Scheduler.h
+++ DijkstraScheduler/Scheduler.h
@@ -8,15 +8,27 @@
 #include <vector>
 #include <map>
 
+#include "Employee.h"
+#include "Role.h"
+
 using namespace std;
+
+vector<string> getVectorFromFileName(string);
+map<string, int> getMapFromVector(vector<string>);
 
 class Scheduler {
 protected:
 	const string roleFile = "roles.xml";
+	const string idFile = "IDs.xml";
 	vector<string> roleVector;
+	vector<string> employeeIDs;
+	vector<Role> roles;
+	vector<Employee> employees;
 	map<string, int> roleMap;
+	map<string, int> idMap;
 public:
 	const map<string, int>& getRoleMap();
+	const map<string, int>& getEmployeeMap();
 	Scheduler();
 	Scheduler(const Scheduler&);
 	virtual ~Scheduler();
